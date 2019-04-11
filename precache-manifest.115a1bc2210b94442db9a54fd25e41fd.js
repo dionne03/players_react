0679dcd40b2da71c77304b5eb1231c29d53432c1
Dionne Gonzalez <dionne.gonzalez.glez@gmail.com>
2019-04-10 20:48:10 -0500

    Updates

---
+++ precache-manifest.115a1bc2210b94442db9a54fd25e41fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/players_react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "10c70476ee8d993b4801",
+    "url": "/players_react/static/js/runtime~main.0955616c.js"
+  },
+  {
+    "revision": "ded49a21b14b32ea1b93",
+    "url": "/players_react/static/js/main.8edb27cf.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/players_react/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "ded49a21b14b32ea1b93",
+    "url": "/players_react/static/css/main.252a1b83.chunk.css"
+  },
+  {
+    "revision": "e0c800fffbf70b7f08af5970c6b7181d",
+    "url": "/players_react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.308e4fd85cd22c1fe61bdb4915500abe.js"
+  "/players_react/precache-manifest.115a1bc2210b94442db9a54fd25e41fd.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/players_react/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
