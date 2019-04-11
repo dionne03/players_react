df22848dc75ba86ee5158eda5572de2f0025028d
Dionne Gonzalez <dionne.gonzalez.glez@gmail.com>
2019-04-10 20:55:53 -0500

    react-app-start2

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code>HOLA
+            Edit <code>src/App.js</code>HOLAcomo estas
           </p>
           <a
             className="App-link"
